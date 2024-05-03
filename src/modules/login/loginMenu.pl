6669227e39ff0ed5dbe86a3a06cc217971f4aa7e
Bruno <bruno.ramos@ccc.ufcg.edu.br>
2024-05-03 19:01:11 -0300

    fix no ponto

--- src/modules/login/loginMenu.pl
+++ src/modules/login/loginMenu.pl
@@ -15,7 +15,7 @@ register(Email, FirstName, LastName, Password, Role):-
     get_one(_, Email) -> write("User already exists");
     insert(Email, FirstName, LastName, Password, true, "", Role, Result),
     write(Result),
-    write("Register Successful")
+    write("Register Successful").
 
 action("1"):-
     write('Enter your email: '), read_string(user_input, '\n', '\r', _, Email),
