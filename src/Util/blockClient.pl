7556fc0f0e17601c40aa9ccd50f5a86cd3aa4b2f
Pedro Jose <pedro123jose321@gmail.com>
2024-05-04 18:19:10 -0300

    blockClient.pl criada

---
+++ src/Util/blockClient.pl
@@ -0,0 +1,4 @@
+blockClient(User, Reason, BlockedUser) :-
+    (Reason = '' ->
+        BlockedUser = User.put(is_active, false).put(block_reason, 'Reason not specified');
+    BlockedUser = User.put(is_active, false).put(block_reason, Reason)).
