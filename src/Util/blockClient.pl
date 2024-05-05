4cfd13fed60e134e214df03f7b11603f84e8a34c
Pedro Jose <pedro123jose321@gmail.com>
2024-05-05 18:03:46 -0300

    blockClient atualizado

--- src/Util/blockClient.pl
+++
@@ -1,6 +0,0 @@
-:- use_module(user)
-
-blockClient(User, Reason, BlockedUser) :-
-    (Reason = '' ->
-        BlockedUser = User.put(is_active, false).put(block_reason, 'Reason not specified');
-    BlockedUser = User.put(is_active, false).put(block_reason, Reason)).
